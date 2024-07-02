fb829636e1526ed4cb52316257743dd73ecdb826
zaky aly <zaky3ly@gmail.com>
2024-07-02 12:22:48 +0200

    adding textStyles and isDisabled to CustomButtonProps

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,8 @@ export interface CustomButtonProps{
     containerStyles?:string;
     handleClick?:MouseEventHandler<HTMLButtonElement>;
     btnType?:'button' | "submit";
+    textStyles?: string;
+    isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps{
