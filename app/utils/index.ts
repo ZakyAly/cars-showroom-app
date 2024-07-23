39ae2435956cf04cf466a3af8e07d92eb1ac3b87
zaky aly <zaky3ly@gmail.com>
2024-07-23 13:44:02 +0200

    passing the FilterProps to the fetchCars function and updating the url

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,14 +1,16 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(){
+export async function fetchCars(filters: FilterProps){
+    const { manufacturer, year, model, limit, fuel}= filters;
+
     const headers = {
             'x-rapidapi-key': '7f3336ea80msh10794ad0384f1f6p11515ajsn41d0772d0257',
             'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
 
     }
     const response = await fetch(
-        'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla'
-        , {headers:headers}
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel=${fuel}`,
+        {headers:headers,},
     ) 
 
     const result = await response.json();
